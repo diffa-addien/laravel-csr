2b519ebe84b4993dc75e776502874a63df99cb99
Diffaddien <diffaddien@gmail.com>
2025-05-16 15:11:42 +0700

    add: change icon

--- app/Filament/Clusters/Sdg.php
+++ app/Filament/Clusters/Sdg.php
@@ -6,7 +6,7 @@ use Filament\Clusters\Cluster;
 
 class Sdg extends Cluster
 {
-    protected static ?string $navigationIcon = 'heroicon-o-squares-2x2';
+    protected static ?string $navigationIcon = 'heroicon-o-flag';
 
     protected static ?string $navigationGroup = 'Data Induk';
     protected static ?string $navigationLabel = 'SDGs';
