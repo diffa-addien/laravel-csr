5fa3c3e1b84509e905dce9f53015567e80916970
diffa-addien <diffadien@gmail.com>
2025-05-27 10:00:29 +0700

    debug: SDGs duplicate logic

--- app/Filament/Clusters/Sdg/Resources/SdgTargetResource.php
+++ app/Filament/Clusters/Sdg/Resources/SdgTargetResource.php
@@ -75,6 +75,7 @@ class SdgTargetResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
