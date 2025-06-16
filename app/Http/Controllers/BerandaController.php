b1fc36c8b066a898b5baae34a38b2e990e34d157
diffaddien <diffaddien@gmail.com>
2025-06-16 10:33:51 +0700

    root redirect to /admin

--- app/Http/Controllers/BerandaController.php
+++ app/Http/Controllers/BerandaController.php
@@ -8,6 +8,7 @@ class BerandaController extends Controller
 {
     public function index()
     {
-        return view('beranda');
+        return redirect('/admin');
+        // return view('beranda');
     }
 }
