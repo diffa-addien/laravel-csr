cfd7e604b3ad113cd2547267d0bf60160eef437e
diffaddien <diffaddien@gmail.com>
2025-06-26 18:07:42 +0700

    change: limit to 5 tahun pengmas

--- app/Filament/Pages/LaporanAnggaran.php
+++ app/Filament/Pages/LaporanAnggaran.php
@@ -30,7 +30,7 @@ class LaporanAnggaran extends Page
         // Query ini untuk menentukan rentang tahun yang akan kita proses.
         $lastFourFiscalYears = TahunFiskal::query()
             ->orderBy('nama_tahun_fiskal', 'desc')
-            ->limit(4)
+            ->limit(5)
             ->get()
             // Urutkan kembali agar data yang ditampilkan kronologis (misal: 2022, 2023, 2024, 2025)
             ->sortBy('nama_tahun_fiskal');
