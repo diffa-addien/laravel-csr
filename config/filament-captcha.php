10add19bb75c2f3c6ebf2df151e27432a390738c
diffaddien <diffaddien@gmail.com>
2025-07-01 18:20:30 +0700

    minor command

--- config/filament-captcha.php
+++ config/filament-captcha.php
@@ -7,13 +7,11 @@ return [
 
     // optional, default is 'abcdefghijklmnpqrstuvwxyz123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     'charset' => '123456789',
-    'fontLines' => 0,
-    'behindLines' => 2,
-
     'width' => 180,
-
     'height' => 50,
-
     'background_color' => [255, 255, 255],
 
+    // INI VENDOR JUGA HARUS DIGANTI
+    'fontLines' => 0,
+    'behindLines' => 2,
 ];
