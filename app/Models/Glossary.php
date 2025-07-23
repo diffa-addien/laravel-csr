55324b1917e8a36ac31c267abad427c9f12cae18
diffaddien <diffaddien@gmail.com>
2025-07-23 13:57:26 +0700

    debug: model glossary

--- app/Models/Glossary.php
+++ app/Models/Glossary.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Glossary extends BaseModel
 {
     use HasFactory;
+    protected $fillable = [
+        'nama_glossary',
+        'keterangan',
+    ];
 }
