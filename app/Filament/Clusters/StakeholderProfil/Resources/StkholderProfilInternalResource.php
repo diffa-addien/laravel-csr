29264b9a87fa075306154db2dc48003d12a23b54
diffaddien <diffaddien@gmail.com>
2025-07-24 16:31:47 +0700

    debug: internal

--- app/Filament/Clusters/StakeholderProfil/Resources/StkholderProfilInternalResource.php
+++ app/Filament/Clusters/StakeholderProfil/Resources/StkholderProfilInternalResource.php
@@ -46,6 +46,12 @@ class StkholderProfilInternalResource extends Resource
                 Forms\Components\TextInput::make('jabatan')
                     ->required()
                     ->maxLength(255),
+                Select::make('kategori_stakeholder_id')
+                    ->relationship(name: 'kategoriStakeholder', titleAttribute: 'nama_kategori')
+                    ->searchable()
+                    ->preload()
+                    ->label('Kategori Stakeholder')
+                    ->required(),
                 Forms\Components\Textarea::make('keterangan')
                     ->required()
                     ->columnSpanFull(),
@@ -61,12 +67,6 @@ class StkholderProfilInternalResource extends Resource
                     ->searchable(),
                 Tables\Columns\TextColumn::make('jabatan')
                     ->searchable(),
-                Select::make('kategori_stakeholder_id')
-                    ->relationship(name: 'kategoriStakeholder', titleAttribute: 'nama_kategori')
-                    ->searchable()
-                    ->preload()
-                    ->label('Kategori Stakeholder')
-                    ->required(),
                 Tables\Columns\TextColumn::make('keterangan')
                     ->searchable()
                     ->limit(50),
